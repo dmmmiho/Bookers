88ec1a93d7d1458342798b2f099276c58c388aba
miho abe <106742176+dmmmiho@users.noreply.github.com>
2022-07-04 13:25:28 +0000

    [update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,17 +7,43 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to 'books/new'
+    redirect_to book_path(book.id)
   end 
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
+  
+  def create
+    book = Book.new(book_params)
+  if book.save
+     redirect_to book_path(book.id)
+  else
+    render :new
+  end 
+  end 
 
   def edit
+    @book = Book.find(params[:id])
   end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
+  end 
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end 
+  
   
   private
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :books
   root 'homes#top'
   post 'books' => 'books#create'
-  get'/top' => 'homes#top'
+  get '/top' => 'homes#top'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
